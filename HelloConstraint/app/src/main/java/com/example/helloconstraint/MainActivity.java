4bd16332d95af80a4169350126d1e8274026280c
Logan Thomas <ltogan.2014@gmail.com>
2019-04-18 03:30:48 -0400

    Convert to data binding

--- HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
+++ HelloConstraint/app/src/main/java/com/example/helloconstraint/MainActivity.java
@@ -1,21 +1,26 @@
 package com.example.helloconstraint;
 
+import android.databinding.DataBindingUtil;
+import android.databinding.ViewDataBinding;
+import android.renderscript.ScriptGroup;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.helloconstraint.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     private int mCount = 0;
-    private TextView mShowCount;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        mShowCount = (TextView) findViewById(R.id.show_count);
+//        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 
     public void showToast(View view) {
@@ -25,13 +30,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void countUp(View view) {
         mCount++;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+        if (binding.showCount != null)
+            binding.showCount.setText(Integer.toString(mCount));
     }
 
     public void setZero(View view) {
         mCount = 0;
-        if (mShowCount != null)
-            mShowCount.setText(Integer.toString(mCount));
+        if (binding.showCount != null)
+            binding.showCount.setText(Integer.toString(mCount));
     }
 }
